068793794c2cb0bb86754cbf494fa3e0f97df245
VarshithReddyBairy <77808640+VarshithReddyBairy@users.noreply.github.com>
2021-03-23 18:57:09 -0500

    bonus section

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var myDataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
+var computation = require('./routes/computation');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/mydata', myDataRouter)
 app.use('/users', usersRouter);
+app.use('/computation',computation)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
